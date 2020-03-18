5631b28992c729f35fddca4215e765c4dd2ded81
DRHood <darryl.r.hood@gmail.com>
2020-03-17 20:59:56 -0400

    server set to run with cocktailRouter, and app

--- server.js
+++ server.js
@@ -1,77 +1,29 @@
-/* 
- * This is the template for a server.js file.  Follow the steps below and read
- * the comments for creating your own (or you can just copy this file).
- */
+const express = require('express');
+const app = express();
 
-/* Step 1
- *
- * Import needed packages
- *
- */
-const express = require('express')
-const app = express()
+// conttroller routes
+const { cocktailRouter } = require('./controllers/cocktails.js');
 
-/* Step 2
- * 
- * import routers from controllers/
- *
- */
-const { templateRouter } = require('./controllers/template.js')
+// MIDDLEWARE
+// parse HTTP requests from URL encoded string
+app.use(express.urlencoded({extended: true}));
+// parse HTTP requests from JSON encoded string
+app.use(express.json());
+// static resource location(css, images, etc.)
+app.use(express.static(`${__dirname}/client/build`));
 
+// routers for application use, first argument is prefix to paths defined in router
+app.use('/api/cocktails', cocktailRouter);
 
-/* Step 3
- *
- * Register middleware...
- */
-
-/* Step 3.a
- * ...to parse the body of the HTTP requests from a URL encoded string 
- */
-app.use(express.urlencoded({extended: true}))
-
-/* Step 3.b 
- *
- * ...to parse the body of the HTTP requests from a JSON string  
- */
-app.use(express.json())
-
-
-/* Step 3.c
- *
- * use the `./client/build` directory to host static resources such as css and
- * image files 
- */
-app.use(express.static(`${__dirname}/client/build`))
-
-
-/* Step 4
- *
- * add router for the application to use. The first argument is a prefix to all
- * the paths defined in the router.
- */
-app.use('/api/helloworld', templateRouter)
-
-/* Step 5
- *
- * add catch all route to serve up the built react app for any request not made to our
- * /api/... routes.
- */
+// route to serve react appfor requests not made to api routes
 app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`)
-})
+    res.sendFile(`${__dirname}/client/build/index.html`);
+});
 
-/* Step 6
- *
- * Set the port the server is to run on
- *
- * NOTE: keep these lines at the bottom of the file 
- */
+
+// server port
 const PORT = process.env.PORT || 3001
-
-/* Step 7
- *
- * Start the server
- */
+// server start
 app.listen(PORT, () => {
-    console.log(`App is listening on PORT ${PORT}`)
-})
+    console.log(`App is listening on PORT ${PORT}`);
+});
