f19e050aaa03bd25b63877089c19d16246b69c0d
DRHood <darryl.r.hood@gmail.com>
2020-03-18 17:52:03 -0400

    App.js initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import HelloWorld from './components/HelloWorld.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Cocktails from './components/Cocktails';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={HelloWorld}/>
+          <Route exact path="/" component={ Cocktails }/>
         </Switch>
       </Router>
     </div>
