6d2a1f2172627ded83e0fba769a9c4737af8d2be
DRHood <darryl.r.hood@gmail.com>
2020-03-19 02:23:50 -0400

    working through cocktails view, and add option

--- client/src/components/Cocktails.jsx
+++ client/src/components/Cocktails.jsx
@@ -1,11 +1,13 @@
 
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class Cocktails extends Component {
 
     state = {
-        cocktails: []
+        cocktails: [],
+        addOption: false,
     }
 
     // componentDidMount to retrieve display data,
@@ -17,12 +19,42 @@ export default class Cocktails extends Component {
         });
     }
 
+    toggleAdd = () => {
+        this.setState({
+            addOption: !this.state.addOption,
+        });
+    }
+
     // render function manages what's shown in browser
     render() {
         return (
             <div>
-                {/* Accessing value of cocktails from state object */}
-                <h1>{this.state.cocktails}</h1>
+                <h1>Cocktails</h1>
+                {
+                    this.state.cocktails.map((cocktail, i) => {
+                        return (
+                            <div key={ i }>
+                                <img src={ cocktail.image }></img>
+                                <br/>
+                                <Link to={ cocktail._id }>{ cocktail.name }</Link>
+                            </div>
+                        )
+                    })
+                }
+                <div>
+                    <button onClick={ this.toggleAdd }>Add Cocktail</button>
+                </div>
+                {
+                    this.state.addOption
+                        ?<form onSubmit={ this.addCocktail }>
+                            <input type="text" name="name" onChange={ this.changeInput }/>
+                            <input type="text" name="glass" onChange={ this.changeInput }/>
+                            <input type="text" name="image" onChange={ this.changeInput }/>
+                            <input type="text" name="ingredients" onChange={ this.changeInput }/>
+                            <input type="text" name="recipe" onChange={ this.changeInput }/>
+                            <input type="submit" value="Add"/>
+                        </form>
+                }
             </div>
         )
     }
